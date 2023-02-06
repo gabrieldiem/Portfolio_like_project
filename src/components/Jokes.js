93bb1ceaa403dc82697cf3ac8492a3e0ff8ae787
walgab <gabriel.diem5@gmail.com>
2023-02-06 11:47:32 -0300

    Added Joke component that used fetch API

---
+++ src/components/Jokes.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react"
+
+const Joke = ({joke: {setup, punchline}}) => (
+    <p style={{margin: 20}}>{setup} <em>{punchline}</em></p>
+)
+
+class Jokes extends Component {
+    state = {initialJoke: {}, extraJokes: []};
+    
+    displayJokes = (event) => {
+        fetch("https://official-joke-api.appspot.com/random_ten")
+        .then(response => response.json())
+        .then(json => this.setState({extraJokes: json}));
+    };
+    
+
+    componentDidMount(){
+        fetch("https://official-joke-api.appspot.com/random_joke")
+        .then(response => response.json())
+        .then(json => this.setState({initialJoke: json}));
+    }
+
+
+    render(){
+        return (<div>
+            <h2>Highlighted joke</h2>
+            <Joke joke={this.state.initialJoke} />
+
+            <h3>Want more jokes?</h3>
+            <button onClick={this.displayJokes}>Click me!</button>
+            {this.state.extraJokes.map(joke => <Joke joke={joke} />)}
+        </div>);
+    }
+}
+
+export default Jokes;
