0ed9a6c469180fed394a73576254be102e0c8eb4
walgab <gabriel.diem5@gmail.com>
2023-02-17 22:15:45 -0300

    Fixed 404 redirect path

---
+++ dist/404.6d2898b8.js
@@ -0,0 +1,2 @@
+var o="localhost"!==window.location.hostname&&!window.location.href.includes("127.0.0.1")?"/Portfolio_like_project":"";window.location.replace(`${o}/`);
+//# sourceMappingURL=404.6d2898b8.js.map
